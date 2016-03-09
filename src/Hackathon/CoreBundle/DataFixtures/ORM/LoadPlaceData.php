b944e2ef3688825224290e0c64f28808c9f7fbc8
Antoine Cusset <a.cusset@gmail.com>
2016-03-09 11:08:09 +0100

    Avis entity + fixtures

--- src/Hackathon/CoreBundle/DataFixtures/ORM/LoadPlaceData.php
+++ src/Hackathon/CoreBundle/DataFixtures/ORM/LoadPlaceData.php
@@ -20,6 +20,7 @@ class LoadPlaceData extends AbstractFixture implements FixtureInterface, Ordered
         $place->setUrlSite('http://www.lecafedeschats.fr');
         $place->setHotel($this->getReference('hotel-bastille'));
         $place->setPlaceType($this->getReference('placetype-bar'));
+        $this->addReference('place-chat',$place);
 
         $place2 = new Place();
         $place2->setName('Chez Janou');
@@ -29,6 +30,7 @@ class LoadPlaceData extends AbstractFixture implements FixtureInterface, Ordered
         $place2->setUrlSite('http://www.chezjanou.com');
         $place2->setHotel($this->getReference('hotel-bastille'));
         $place2->setPlaceType($this->getReference('placetype-gastronomie'));
+        $this->addReference('place-janou',$place2);
 
         $place3 = new Place();
         $place3->setName('Comédie Bastille');
@@ -38,6 +40,7 @@ class LoadPlaceData extends AbstractFixture implements FixtureInterface, Ordered
         $place3->setUrlSite('http://www.comedie-bastille.com');
         $place3->setHotel($this->getReference('hotel-bastille'));
         $place3->setPlaceType($this->getReference('placetype-culture'));
+        $this->addReference('place-comedie',$place3);
 
         $place4 = new Place();
         $place4->setName('Piscine de la Cour des Lions');
@@ -47,6 +50,7 @@ class LoadPlaceData extends AbstractFixture implements FixtureInterface, Ordered
         $place4->setUrlSite('http://equipement.paris.fr');
         $place4->setHotel($this->getReference('hotel-bastille'));
         $place4->setPlaceType($this->getReference('placetype-sport'));
+        $this->addReference('place-lions',$place4);
 
         $objectManager->persist($place);
         $objectManager->persist($place2);
@@ -55,14 +59,6 @@ class LoadPlaceData extends AbstractFixture implements FixtureInterface, Ordered
 
         /********************************************************************/
 
-        $place = new Place();
-        $place->setName('L\'Express de Lyon');
-        $place->setDescription('Café-bar au cadre sobre et convivial, réputé pour sa sélection de bières pression et ses plats typiques.');
-        $place->setAddress('1 Rue de Lyon, 75012 Paris');
-        $place->setTel('01 43 43 21 32');
-        $place->setUrlSite('');
-        $place->setHotel($this->getReference('hotel-aurore'));
-        $place->setPlaceType($this->getReference('placetype-bar'));
         $place5 = new Place();
         $place5->setName('L\'Express de Lyon');
         $place5->setDescription('Café-bar au cadre sobre et convivial, réputé pour sa sélection de bières pression et ses plats typiques.');
@@ -71,15 +67,8 @@ class LoadPlaceData extends AbstractFixture implements FixtureInterface, Ordered
         $place5->setUrlSite('');
         $place5->setHotel($this->getReference('hotel-aurore'));
         $place5->setPlaceType($this->getReference('placetype-bar'));
+        $this->addReference('place-express',$place5);
 
-        $place2 = new Place();
-        $place2->setName('L\'Européen');
-        $place2->setDescription('Brasserie au décor rétro et design, avec lustres en verrine et miroirs, spécialisée dans les fruits de mer.');
-        $place2->setAddress('21 bis Boulevard Diderot, 75012 Paris');
-        $place2->setTel('01 43 43 99 70');
-        $place2->setUrlSite('http://www.l-europeen.com/');
-        $place2->setHotel($this->getReference('hotel-aurore'));
-        $place2->setPlaceType($this->getReference('placetype-gastronomie'));
         $place6 = new Place();
         $place6->setName('L\'Européen');
         $place6->setDescription('Brasserie au décor rétro et design, avec lustres en verrine et miroirs, spécialisée dans les fruits de mer.');
@@ -88,15 +77,8 @@ class LoadPlaceData extends AbstractFixture implements FixtureInterface, Ordered
         $place6->setUrlSite('http://www.l-europeen.com/');
         $place6->setHotel($this->getReference('hotel-aurore'));
         $place6->setPlaceType($this->getReference('placetype-gastronomie'));
+        $this->addReference('place-europeen',$place6);
 
-        $place3 = new Place();
-        $place3->setName('La Maison Rouge');
-        $place3->setDescription('Établissement qui pèse de ouf - Jolan, 2014');
-        $place3->setAddress('10 Boulevard de la Bastille, 75012 Paris');
-        $place3->setTel('01 40 01 08 80');
-        $place3->setUrlSite('http://www.lamaisonrouge.org');
-        $place3->setHotel($this->getReference('hotel-aurore'));
-        $place3->setPlaceType($this->getReference('placetype-culture'));
         $place7 = new Place();
         $place7->setName('La Maison Rouge');
         $place7->setDescription('Établissement qui pèse de ouf - Jolan, 2014');
@@ -105,15 +87,8 @@ class LoadPlaceData extends AbstractFixture implements FixtureInterface, Ordered
         $place7->setUrlSite('http://www.lamaisonrouge.org');
         $place7->setHotel($this->getReference('hotel-aurore'));
         $place7->setPlaceType($this->getReference('placetype-culture'));
+        $this->addReference('place-maison',$place7);
 
-        $place4 = new Place();
-        $place4->setName('Piscine Joséphine Baker');
-        $place4->setDescription('Une piscine parisienne sur une péniche, au milieu de la seine');
-        $place4->setAddress('Port de la Gare, Quai François Mauriac, 75013 Paris');
-        $place4->setTel('01 56 61 96 503');
-        $place4->setUrlSite('http://carilis.fr');
-        $place4->setHotel($this->getReference('hotel-aurore'));
-        $place4->setPlaceType($this->getReference('placetype-sport'));
         $place8 = new Place();
         $place8->setName('Piscine Joséphine Baker');
         $place8->setDescription('Une piscine parisienne sur une péniche, au milieu de la seine');
@@ -122,6 +97,7 @@ class LoadPlaceData extends AbstractFixture implements FixtureInterface, Ordered
         $place8->setUrlSite('http://carilis.fr');
         $place8->setHotel($this->getReference('hotel-aurore'));
         $place8->setPlaceType($this->getReference('placetype-sport'));
+        $this->addReference('place-baker',$place8);
 
         $objectManager->persist($place5);
         $objectManager->persist($place6);
---
+++ src/Hackathon/CoreBundle/Entity/Avis.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace Hackathon\CoreBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Avis
+ *
+ * @ORM\Table(name="avis")
+ * @ORM\Entity(repositoryClass="Hackathon\CoreBundle\Repository\AvisRepository")
+ */
+class Avis
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="datetimetz")
+     */
+    private $date;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="note", type="float")
+     */
+    private $note;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Place")
+     * @ORM\JoinColumn(name="place_id", referencedColumnName="id")
+     */
+    private $place;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     * @return Avis
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime 
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Avis
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set note
+     *
+     * @param float $note
+     * @return Avis
+     */
+    public function setNote($note)
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+
+    /**
+     * Get note
+     *
+     * @return float 
+     */
+    public function getNote()
+    {
+        return $this->note;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPlace()
+    {
+        return $this->place;
+    }
+
+    /**
+     * @param mixed $place
+     */
+    public function setPlace($place)
+    {
+        $this->place = $place;
+    }
+}
