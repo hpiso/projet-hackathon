23076980fd604d158f9aeae73ed8257329707258
hpiso <hugopiso@gmail.com>
2016-03-09 11:38:44 +0100

    form filter

--- src/Hackathon/FrontBundle/Controller/DefaultController.php
+++ src/Hackathon/FrontBundle/Controller/DefaultController.php
@@ -2,16 +2,27 @@
 
 namespace Hackathon\FrontBundle\Controller;
 
+use Hackathon\CoreBundle\Form\FilterType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('HackathonFrontBundle:Default:index.html.twig');
+        $form = $this->createForm(new FilterType(), null);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            dump($form);die;
+        }
+
+        return $this->render('HackathonFrontBundle:Default:index.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
