de7c01cc61662fdcac9601e26763a001bc65b78d
Alexandre Morin <morinalexandrem@gmail.com>
2016-03-09 17:00:31 +0100

    Function search hotel

---
+++ src/Hackathon/CoreBundle/Form/FilterType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Hackathon\CoreBundle\Form;
+
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class FilterType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('Recherche', EntityType::class, [
+                'class' => 'HackathonCoreBundle:Hotel',
+                'choice_label' => 'name',
+                'label' => false
+            ])
+        ;
+    }
+
+}
--- src/Hackathon/FrontBundle/Controller/DefaultController.php
+++ src/Hackathon/FrontBundle/Controller/DefaultController.php
@@ -2,16 +2,28 @@
 
 namespace Hackathon\FrontBundle\Controller;
 
+use Hackathon\CoreBundle\Form\FilterType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('HackathonFrontBundle:Default:index.html.twig');
+        $form = $this->createForm(new FilterType(), null);
+        $form->handleRequest($request);
+        $hotel = null;
+        if ($form->isValid()) {
+            $hotel = $form->getData();
+        }
+
+        return $this->render('HackathonFrontBundle:Default:index.html.twig', [
+            'form' => $form->createView(),
+            'hotel' => $hotel
+        ]);
     }
 }
